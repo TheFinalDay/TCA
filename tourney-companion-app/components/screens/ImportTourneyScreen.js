6b9bc0c4ed234e71a6ac78a4050d3c3660441ae4
TheFinalDay <adrien.825@gmail.com>
2020-07-25 22:40:56 -0400

    node_modules reload and some minor adjustments

--- tourney-companion-app/components/screens/ImportTourneyScreen.js
+++ tourney-companion-app/components/screens/ImportTourneyScreen.js
@@ -1,7 +1,44 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
+// test bracket url: https://challonge.com/picfs9jq
+
 const ImportTourneyScreen = props => {
+
+    // TODO - IN ORDER:
+    // - get tourney URL from user
+    // - get players list from API
+    // - get user associated contestant with dropdown list
+    // - switch to dashboard and do all the GET requests for the bracket there...
+
+    /**
+    
+    public async sendRequest(keyword: string): Promise<Array<Object>>{
+        
+        var formatted_keyword = keyword.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        var myHeaders = new Headers();
+		var requestOptions: RequestInit = { method: 'GET', headers: myHeaders, redirect: 'follow' };
+		try {
+			let response = await fetch(`https://prod-19.canadacentral.logic.azure.com/workflows/
+					7810e8b05a0c4da197f91c5088f74e05/triggers/manual/paths/invoke/nomEntreprise/${formatted_keyword}?
+					api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&
+					sig=oJ-WjSJRhEpRbUnolxruhPj6PL2bgeYZ_LXCV1oG0Ys`, requestOptions)
+			if (response.status !== 200) { 
+                console.log(`${response.status} ${response.statusText} | ${response.url}`); 
+                let responsetext = await response.text();
+                if(responsetext == "Erreur : Plus de 500 résultats précisez votre recherche svp."){
+                    this.updateIsDataOverloadHandler(true);
+                }
+                return []; 
+            }
+            let responsetext = await response.text();
+            let items = JSON.parse(responsetext).value;
+			return items;
+		} catch (e) { throw e; }
+    }
+
+     */
+
     return(
         <View>
             <Text>Import Tourney Screen</Text>
@@ -9,6 +46,9 @@ const ImportTourneyScreen = props => {
     );
 }
 
+ImportTourneyScreen.navigationOptions = {
+    headerTitle: "New Tourney"
+}
 
 const styles = StyleSheet.create({
 
--- tourney-companion-app/navigation/drawers/MainNavigator.js
+++ tourney-companion-app/navigation/drawers/MainNavigator.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Ionicons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 import TourneyTabNavigator from '../tabs/TourneyTabNavigator';
 import ImportTourneyScreen from '../../components/screens/ImportTourneyScreen';
 
 const MainNavigator = createDrawerNavigator({
-    CurrentTourney: {
-        screen: TourneyTabNavigator,
-        navigationOptions: {
-            drawerLabel: 'Meals'
-        }
-    },
     Import: {
         screen: ImportTourneyScreen,
         navigationOptions: {
-            drawerLabel: 'Import'
+            drawerLabel: 'Join New Tourney'
         }
     },
+    CurrentTourney: {
+        screen: TourneyTabNavigator,
+        navigationOptions: {
+            drawerLabel: 'Active Dashboard'
+        }
+    },
+    
+    
+    
+    
     
     /** TODO: add the other drawer options...
     ,
@@ -31,14 +35,15 @@ const MainNavigator = createDrawerNavigator({
     }
 
     */
-}, {
-    contentOptions: {
-        labelStyle: {
-            fontFamily: 'prototype'
+}, 
+    {
+        contentOptions: {
+            labelStyle: {
+                fontFamily: 'prototype'
+            }
         }
     }
-})
 
-
+)
 
 export default createAppContainer(MainNavigator);
