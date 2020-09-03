88749d639094d5c6d044f2a6256ead0a38aa6c07
Adrien Champagne <an61110@etsmtl.net>
2020-09-03 17:58:15 -0400

    added some api calls to firebase

--- tourney-companion-app/components/screens/TourneyInfoScreen.js
+++ tourney-companion-app/components/screens/TourneyInfoScreen.js
@@ -4,14 +4,25 @@ import { Text, StyleSheet, View, Dimensions, TouchableOpacity } from 'react-nati
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { DeepBlue } from '../../constants/Colors';
+import SimpleButton from '../UI/SimpleButton';
+import { API } from '../../misc/apiCalls';
 
 const TourneyInfoScreen = props => {
+
+    const tourney = useSelector(state => state.tournaments.activeTournament);
 
 
     return(
         <View style={styles.screen}>
             <Text style={{color: DeepBlue.primary_light}}>Tourney Info Screen</Text>
             <Text style={{color: DeepBlue.primary_light}}>Coming soon, but not that soon ya know!</Text>
+            <SimpleButton onPress={() => {
+                if(tourney){
+                    API._registerTourney(tourney.tid).then(result => {
+                        console.log("id: "+result.payloadData.id +"& tid: "+result.payloadData.tid);
+                    });
+                }
+            }}>Register tourney in DB</SimpleButton>
         </View>
     );
 
--- tourney-companion-app/misc/apiCalls.js
+++ tourney-companion-app/misc/apiCalls.js
@@ -112,8 +112,50 @@ const _getMatchList = async (url) => {
     };
 };
 
+const _registerTourney = async (tid) => {
+
+    let resData = null;
+
+    if (tid == '') {
+        return;
+    }
+
+    try {
+        const response = await fetch(
+            `https://tca-testing.firebaseio.com/registeredtourneys.json`, 
+                {
+                method: 'POST',
+                header: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    tid
+                })
+            }
+        );
+
+        if (response.status !== 200) { 
+            console.log(`${response.status} ${response.statusText} | ${response.url}`); 
+            return;
+        } else {
+            resData = await response.json();
+        }
+
+        console.log(resData);
+
+    } catch (e) { throw e; }
+
+    return {
+        payloadData: {
+            id: resData.name,
+            tid: tid
+        }
+    };
+};
+
 export const API = {
     _getPlayerList,
     _getMatchList,
-    _getTournamentInfo
+    _getTournamentInfo,
+    _registerTourney
 }
