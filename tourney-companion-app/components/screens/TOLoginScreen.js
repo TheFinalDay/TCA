6fd5d17be40d28621b206feec9b447d3c346c45c
Adrien Champagne <an61110@etsmtl.net>
2020-09-03 12:18:06 -0400

    TO Login empty screen

---
+++ tourney-companion-app/components/screens/TOLoginScreen.js
@@ -0,0 +1,25 @@
+
+import { DeepBlue } from '../../constants/Colors';
+
+const TOLoginScreen = props => {
+
+
+    return(
+        <View style={styles.screen}>
+            <Text style={{color: DeepBlue.primary_light}}>TO Login Screen</Text>
+            <Text style={{color: DeepBlue.primary_light}}>Coming soon!</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: DeepBlue.bg_primary
+    }
+});
+
+export default TOLoginScreen;
